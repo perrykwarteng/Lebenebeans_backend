c9e9d32e3a5cf3ca9badc8b99e4ff595b6315fc6
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-09 14:12:46 +0000

    Feat: Added logout and me endpoint

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, logout, me, register } from "../controllers/auth.controller.js";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.get("/me", requireAuth, me);
+router.post("/logout", logout);
 
 export default router;
