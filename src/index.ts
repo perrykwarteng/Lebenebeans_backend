e482b2c09e5ad5db28afc9fd055e8ec7d97f667b
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-09 13:58:44 +0000

    Feat: Added websocket for streaming data to the frontend with a new order is made

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,22 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 import orderRouter from "./routes/orders.route.js";
+import locationRouter from "./routes/location.route.js";
+import statisticsRouter from "./routes/statistics.route.js";
+import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 const app = express();
+const server = http.createServer(app);
+
+export const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  },
+});
+
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
@@ -14,7 +27,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/orders", orderRouter);
+app.use("/api/locations", locationRouter);
+app.use("/api/statistics", statisticsRouter);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  socket;
+});
+
+server.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
 });
