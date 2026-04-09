39e694047bddc3117695f1f8aeece096754bddb0
Perry Kwarteng <perrykwarteng8@gmail.com>
2026-04-09 14:15:05 +0000

    Feat: Added delivered , pending, closeOrderStatus, orderStatus, webhook to order routes

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,12 +1,27 @@
 import { Router } from "express";
 import {
+  cancelStatus,
+  changeOrderStatus,
+  closeOrder,
   createOrders,
   verifyTransaction,
+  deliveredOrders,
+  deliveredStatus,
+  pendingOrders,
+  webhook,
 } from "../controllers/orders.controller.js";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/orders", createOrders);
+router.get("/pendingOrders", requireAuth, pendingOrders);
+router.get("/deliveredOrders", requireAuth, deliveredOrders);
+router.get("/orderDelivered/:id", requireAuth, deliveredStatus);
+router.get("/orderCancle/:id", requireAuth, cancelStatus);
 router.get("/verify/:reference", verifyTransaction);
+router.post("/webhook", webhook);
+router.get("/closeOrder", closeOrder);
+router.patch("/changeOrderStatus/:id", requireAuth, changeOrderStatus);
 
 export default router;
